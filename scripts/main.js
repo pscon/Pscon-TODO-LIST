7ccc1cf1cdc995ca6650ae096517b654ddbd9912
pscon <pscontech@gmail.com>
2021-09-08 09:09:35 +0100

    first todo list

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+alert('Welcome to Pscon To-Do List');
+
+$(function() {
+
+    var $list, $newItemForm;
+    $list = $('ul');
+    $newItemForm = $('#newItemForm');
+  
+    $newItemForm.on('submit', function(e) {
+      e.preventDefault();
+      var text = $('input:text').val();
+    
+      $list.append('<li>' + text + '</li>');
+      $('input:text').val('');
+    });
+  
+    $list.on('click', 'li', function() {
+      var $this = $(this);
+      $this.remove();
+    });
+  
+  });
